3eae1d9f802973f391777e9a0a074ca3f2321dad
Angelo <angelo.cina@yahoo.it>
2020-12-03 18:28:59 +0100

    inizio Milestone 4

--- js/script.js
+++ js/script.js
@@ -8,22 +8,28 @@ var app = new Vue ({ //instanzio vue
   },
   methods: {
     cercaFilm: function () {
-      let movies = 'https://api.themoviedb.org/3/search/movie?api_key=00fc6f8f5568da14692dfb2724b20a69&language=it-IT';
-      let series = 'https://api.themoviedb.org/3/search/tv?api_key=00fc6f8f5568da14692dfb2724b20a69&language=it-IT';
+      if (this.inputSearch != "") {
+        let movies = 'https://api.themoviedb.org/3/search/movie?api_key=00fc6f8f5568da14692dfb2724b20a69&language=it-IT';
+        let series = 'https://api.themoviedb.org/3/search/tv?api_key=00fc6f8f5568da14692dfb2724b20a69&language=it-IT';
 
-      let requestMovies = axios.get(movies, {params: {query: this.inputSearch}});
-      let requestSeries = axios.get(series, {params: {query: this.inputSearch}});
+        let requestMovies = axios.get(movies, {params: {query: this.inputSearch}});
+        let requestSeries = axios.get(series, {params: {query: this.inputSearch}});
 
-      axios.all([requestMovies, requestSeries])
-      .then(axios.spread((...responses) => {
-        this.moviesDb = responses[0].data.results;
-        console.log('THIS.MOVIESDB', this.moviesDb)
-        this.seriesDb = responses[1].data.results;
-        console.log('THIS.SERIESDB', this.seriesDb)
-        this.roundRate();
-        this.languageFlag();
-        this.resizePoster();
-      }))
+        axios.all([requestMovies, requestSeries])
+        .then(axios.spread((...responses) => {
+          this.moviesDb = responses[0].data.results;
+          console.log('THIS.MOVIESDB', this.moviesDb)
+          this.seriesDb = responses[1].data.results;
+          console.log('THIS.SERIESDB', this.seriesDb)
+          this.roundRate();
+          this.languageFlag();
+          this.resizePoster();
+        }))
+      } else {
+        this.moviesDb = [];
+        this.seriesDb = [];
+      }
+
     },
     roundRate: function () {
       this.moviesDb.forEach((item) => {
