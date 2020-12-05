81b06f82dade4bd372d7f4d604340b8f37f6a4c5
Angelo <angelo.cina@yahoo.it>
2020-12-05 12:19:47 +0100

    Milestone 5, refactoring and layout

--- bonus/js/script.js
+++ bonus/js/script.js
@@ -23,7 +23,7 @@ var app = new Vue ({
           this.seriesDb = responses[1].data.results;
           this.arrayManagement();
           this.totalDb = this.unioneArray();
-          console.log('THIS.TOTALDB', this.totalDb);
+          // console.log('THIS.TOTALDB', this.totalDb);
         }))
       } else {
         this.moviesDb = [];
@@ -54,9 +54,15 @@ var app = new Vue ({
         this.genres = [];
         axios.get("https://api.themoviedb.org/3/movie/" + id + "?api_key=00fc6f8f5568da14692dfb2724b20a69&language=it-IT&append_to_response=credits")
         .then(risposta => {
-          for (var i = 0; i < 5; i++) {
-            this.cast.push(risposta.data.credits.cast[i]);
-          }
+          // for (var i = 0; i < 5; i++) {
+          //   this.cast.push(risposta.data.credits.cast[i].name);
+          //   this.genres.push(risposta.data.genres[i].name);
+          // }
+          risposta.data.credits.cast.forEach((item, i) => {
+            if (i<5) {
+              this.cast.push(item.name);
+            }
+          });
           risposta.data.genres.forEach((item, i) => {
             if (i<4) {
               this.genres.push(item.name);
@@ -68,9 +74,15 @@ var app = new Vue ({
         this.genres = [];
         axios.get("https://api.themoviedb.org/3/tv/" + id + "?api_key=00fc6f8f5568da14692dfb2724b20a69&language=it-IT&append_to_response=credits")
         .then(risposta => {
-          for (var i = 0; i < 5; i++) {
-            this.cast.push(risposta.data.credits.cast[i]);
-          }
+          // for (var i = 0; i < 5; i++) {
+          //   this.cast.push(risposta.data.credits.cast[i].name);
+          //   this.genres.push(risposta.data.genres[i].name);
+          // }
+          risposta.data.credits.cast.forEach((item, i) => {
+            if (i<5) {
+              this.cast.push(item.name);
+            }
+          });
           risposta.data.genres.forEach((item, i) => {
             if (i<4) {
               this.genres.push(item.name);
